393a179fe462ba0482c08bacea2f682bcb499c31
Kyle Mellander <kmellander@focusvision.com>
2017-09-15 22:50:52 -0700

    Test falsy paths of isDOMRect

--- test/test.js
+++ test/test.js
@@ -8,4 +8,16 @@ describe('isDOMRect()', function() {
   it('returns true if element has function #getBoundingClientRect', () => {
     assert.ok(isDOMRect({ getBoundingClientRect: () => {} }));
   });
+
+  it('returns falsy if element does not exist', () => {
+    assert.ok(!isDOMRect());
+  });
+
+  it('returns falsy if element#getBoundingClientRect is undefined', () => {
+    assert.ok(!isDOMRect({ getSomethingElse: () => {} }));
+  });
+
+  it('returns false if element#getBoundingClientRect is not a function', () => {
+    assert.equal(isDOMRect({ getBoundingClientRect: 10 }), false);
+  });
 });
