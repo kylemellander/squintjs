7813da139caa4eb484e658990099ee6a858f4bc6
Kyle Mellander <kmellander@focusvision.com>
2017-09-15 22:52:16 -0700

    Add code for adjustByPercent

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+export const adjustByPercent = (bounds, key, percent) => {
+  const height = bounds.bottom - bounds.top;
+  const adjustmentValue = height * percent / 100;
+  
+  return bounds[key] + adjustmentValue;
+}
+
 export const isDOMRect = element =>
   element && typeof element.getBoundingClientRect === 'function';
