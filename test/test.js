8d7bf5385ca82a0b8717b06749819056d42263f5
Kyle Mellander <kmellander@focusvision.com>
2017-09-16 17:42:53 -0700

    Add more tests for offsetBoundary()

--- test/test.js
+++ test/test.js
@@ -42,4 +42,28 @@ describe('offsetBoundary()', () => {
   it('returns the value of the key put in if offset is not set', () => {
     assert.equal(offsetBoundary({ key: 'value' }, 'key'), 'value');
   });
+
+  it('returns the value plus the offset if it is an integer', () => {
+    assert.equal(offsetBoundary({ key: 10 }, 'key', 10), 20);
+  });
+
+  it('returns the value plus the offset if it is a number as a string', () => {
+    assert.equal(offsetBoundary({ key: 10 }, 'key', '10'), 20);
+  });
+
+  it('returns value plus percent of the height if % string is sent in', () => {
+    assert.equal(offsetBoundary({ key: 10, height: 10 }, 'key', '10%'), 11);
+  });
+
+  describe('with modifier of -1', () => {
+    it('returns value minus the offset', () => {
+      assert.equal(offsetBoundary({ key: 10 }, 'key', '10', -1), 0);
+    });
+
+    it('returns value minus percent of the height if % string given', () => {
+      const result = offsetBoundary({ key: 10, height: 10 }, 'key', '10%', -1);
+
+      assert.equal(result, 9);
+    });
+  });
 });
